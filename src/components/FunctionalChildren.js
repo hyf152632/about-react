cba6c0a73e6f26e8b2acf4fe119f1ac957779fc9
yongfei huo <hyfpersonal@outlook.com>
2019-03-03 11:26:38 +0800

    add recurison

--- src/components/FunctionalChildren.js
+++ src/components/FunctionalChildren.js
@@ -23,3 +23,20 @@ const FunctionalChildrenShowName = () => (
 );
 
 export default FunctionalChildrenShowName;
+
+// Calls the children callback numTimes to produce a repeated component
+function Repeat(props) {
+  let items = [];
+  for (let i = 0; i < props.numTimes; i++) {
+    items.push(props.children(i));
+  }
+  return <div>{items}</div>;
+}
+
+function ListOfTenThings() {
+  return (
+    <Repeat numTimes={10}>
+      {index => <div key={index}>This is item {index} in the list</div>}
+    </Repeat>
+  );
+}
